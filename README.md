aea493be0bd55b8e4a27de0ef9d3d1270755c5b6
nataschaberg <natalja.wittenberg@gmx.de>
2023-02-20 10:54:34 +0100

    setup plain

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # faces-emotions
+todo
---
+++ app.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+import streamlit as st
+
+
+st.header('Image Classification - Facial Expressions')
+st.subheader('Classification of emotions based on facial expressions - underlying data set FER-2013')
+‚
